1db55ce386e9309d7afde2e32af394f3f8fbc387
vivekbabu123 <rajkmr98752@gmail.com>
2024-04-30 18:08:26 +0530

    create project

---
+++ index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KAMAL POLYPLAST</title>
+    <!-- link custom css link -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <!-- Boxicons JS -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+</head>
+
+<body>
+
+    <!-- top header start -->
+    <header class="top_header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="main_top">
+                        <div class="left_top">
+                            <ul>
+                                <li><a href="#"><i class='bx bx-envelope'></i><span>shankar@kamalpolyplast.com</span></a></li>
+                                <li><a href="#"><i class='bx bx-phone'></i><span>+91 - 8056246563</span></a></li>
+                            </ul>
+
+                        </div>
+                        <div class="right_top">
+                            <ul>
+                                <li><a href="#"><i class='bx bxl-facebook'></i></a></li>
+                                <li><a href="#"><i class='bx bxl-twitter'></i></a></li>
+                                <li><a href="#"><i class='bx bxl-linkedin'></i></a></li>
+
+                            </ul>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </header>
+    <!-- top header end -->
+
+    <!-- navbar start -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-White py-4">
+        <div class="container-fluid px-4">
+            <a class="navbar-brand" href="#">
+                <img src="./assets/image/logo/logo-default.png" alt="img" class="img-fluid">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-nowrap" href="#">COMPANY PROFILE</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            PLASTIC GRANULES
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">QUALITY</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CARRER</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">CONTACT</a>
+                    </li>
+                </ul>
+                <form class="d-flex menu_btn">
+                    <button class="btn text-white fw-bold text-nowrap" type="submit">Quick Query</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+
+    <!-- navbar finish -->
+
+    <!-- hero slider start -->
+
+    <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <img src="./assets/image/banner/banner1.jpg" />
+            </div>
+            <div class="swiper-slide">
+                <img src="./assets/image/banner/banner2.jpg" />
+            </div>
+            <div class="swiper-slide">
+                <img src="./assets/image/banner/banner3.jpg" />
+            </div>
+            <div class="swiper-slide">
+                <img src="./assets/image/banner/banner1.jpg" />
+            </div>
+
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+    </div>
+    <!-- hero slider finish -->
+
+    <!-- kamal polyplast start -->
+    <section class="kamal_polyplast">
+        <div class="container position-relative">
+            <div class="row">
+                <div class="col-12">
+                    <div class="sub-title">
+                        <p>KAMAL POLYPLAST</p>
+
+                    </div>
+                    <div class="title">
+                        <h2 class="text-center">Welcome To KAMAL POLYPLAST</h2>
+                        <hr class="title_bottom">
+                    </div>
+
+
+
+                </div>
+            </div>
+            <div class="vc_empty_space" style="height: 78px"><span class="vc_empty_space_inner"></span></div>
+            <div class="slz-shortcode sc_service sc_style_icon_9  goahead_service-1019472926588595704d0e7">
+                <div class="content-accountant-service ">
+                    <div class="row">
+                        <div class="block-col-3 col-lg-4 col-md-4 inline_block">
+                            <div class="main-accountant-service">
+                                <div class="title1"><a class="main-menu title1" href="#">Infrastructure</a></div>
+                                <div class="service-salary "> <img src="./assets/image/case-1.png" style="margin-bottom:15px;">
+                                    <div class="text">The manufacturing unit and office of the company are located in the Bangalore, Chennai &amp; Delhi. The unit is supported with state-of-the-art machinery for manufacturing of the Plastic Granules Lorem ipsum dolor, sit amet </div>
+                                    <div class="btnreadmore"><a class="main-menu readmoreabslut" href="Company-Profile.php" data-modal="modal-12">Learn More</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block-col-3 col-lg-4 col-md-4  inline_block ">
+                            <div class="main-accountant-service">
+                                <div class="title1"><a href="#" class="title1">Team</a></div>
+                                <div class="service-salary "> <img src="./assets/image/case-2.png" style="margin-bottom:15px;">
+                                    <div class="text">Forming the backbone of the company, the team at KAMAL POLYPLAST is always ready to rise up to the challenging market demands. The team members involved in production, quality control and delivery...</div>
+                                    <div class="btnreadmore"><a href="Company-Profile.php" class="readmoreabslut">Learn More</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block-col-3 col-lg-4 col-md-4  inline_block ">
+                            <div class="main-accountant-service">
+                                <div class="title1"><a href="#" class="title1">Quality Assurance</a></div>
+                                <div class="service-salary "> <img src="./assets/image/case-3.png" style="margin-bottom:15px;">
+                                    <div class="text">We have a keen understanding of the quality levels expected by the clientele and accordingly undertake the strictest checking at all levels to ensure the best. Right from sourcing high grade raw material...</div>
+                                    <div class="btnreadmore"><a href="QualityPolicy.php" class="readmoreabslut">Learn More</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- kamal polyplast finish -->
+    <!-- Our product start -->
+    <section class="Our_product">
+        <div class="container position-relative">
+            <div class="row">
+                <div class="col-12">
+                    <div class="product-subtitle">
+                        <p>PLASTIC GRANULES</p>
+
+                    </div>
+                    <div class="product-title1">
+                        <h2 class="text-center">OUR PRODUCTS</h2>
+                        <hr class="title_bottom">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </section>
+
+    <!-- Our product finish -->
+
+
+
+
+
+
+    <!-- bootstrap js link -->
+
+    <script src="./assets/js/bootstrap.bundle.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="./assets/js/main.js"></script>
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+    <script>
+        var swiper = new Swiper(".mySwiper", {
+            spaceBetween: 30,
+            effect: "fade",
+            loop: true,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+        });
+    </script>
+
+</body>
+
+</html>
